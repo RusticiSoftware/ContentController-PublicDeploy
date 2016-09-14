49b7979867d3abf12e6025c348430bf37da13f3a
John Mensel <john.mensel@scorm.com>
2016-09-14 08:54:48 -0500

    Created 0.9.207b orphan branch for public deployment

---
+++ link-vars.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd group_vars
+
+ln -fs ../private/group_vars/aws.yml aws.yml
+ln -fs ../private/group_vars/cloudfront.yml cloudfront.yml
+ln -fs ../private/group_vars/content_controller.yml content_controller.yml
+ln -fs ../private/group_vars/engine_java.yml engine_java.yml
+ln -fs ../private/group_vars/env.yml env.yml
+ln -fs ../private/group_vars/s3.yml s3.yml
+ln -fs ../private/group_vars/keypair.yml keypair.yml
+
+cd ../host_vars
+
+cp ../private/host_vars/* .
+
+cd ../roles
+
+if [ -d ../private/roles/users ]; then
+	ln -fs ../private/roles/users .
+fi
+
+cp -rp ../private/roles/ssl/files ssl/files
+
+cp -rp ../private/roles/cloudfront/files cloudfront/files
+
+if [ -d ../private/roles/logstash-filebeat ]; then
+	ln -fs ../private/roles/logstash-filebeat .
+fi
+if [ -d ../private/roles/dripstat ]; then
+	ln -fs ../private/roles/dripstat .
+fi
