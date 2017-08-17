514f47b867d4ccf3bb27bf196d2a2b0ca245d00f
jenkins <jenkins@rusticiscoftware.com>
2017-08-17 20:29:11 +0000

    v1.2.315 Release

---
+++ link-vars-dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ ! -d ../cc-deployment-dev ]; then
+    echo "Please clone cc-deployment-dev to the same directory as cc-deployment-public before running this script."
+    exit 1
+fi
+
+# Link group vars
+cd group_vars
+for file in `find ../../cc-deployment-dev/group_vars -name '*.yml' -type f -exec basename {} ';'`; do
+    ln -fs "../../cc-deployment-dev/group_vars/$file" "$file"
+done
+cd ..
+
+# Link host vars
+cd host_vars
+for file in `find ../../cc-deployment-dev/host_vars -name '*.yml' -type f -exec basename {} ';'`; do
+    ln -fs "../../cc-deployment-dev/host_vars/$file" "$file"
+done
+cd ..
